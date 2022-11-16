eb092565fb9f87f338411fe8d481175d55258599
Luiz Segundo <simoes.segundo@gmail.com>
2022-11-15 21:20:34 -0300

    Adicionando saldo em conta corrento

--- mini-banco/entities/checking-account.go
+++ mini-banco/entities/checking-account.go
@@ -6,12 +6,14 @@ import (
 
 type CheckingAccount struct {
 	Tranasctions []Transaction
+	TotalAccount float64
 }
 
 func (c *CheckingAccount) NoticeTranasaction(newTransaction Transaction) {
 	fmt.Printf("%f\n", newTransaction.Amount)
-	checkTotalLessZero(newTransaction, c.Tranasctions)
+	checkTotalLessZero(newTransaction, c.TotalAccount)
 	c.Tranasctions = append(c.Tranasctions, newTransaction)
+	c.TotalAccount += newTransaction.Amount
 }
 
 func (c *CheckingAccount) Total() float64 {
@@ -22,15 +24,9 @@ func (c *CheckingAccount) Total() float64 {
 	return total
 }
 
-func checkTotalLessZero(newTransaction Transaction, transactions []Transaction) {
-	var total float64 = 0
-	for _, transaction := range transactions {
-		total += transaction.Amount
-	}
-
-	total += newTransaction.Amount
-
-	if total < 0 {
+func checkTotalLessZero(newTransaction Transaction, total float64) {
+	checkValue := total + newTransaction.Amount
+	if checkValue < 0 {
 		panic("A conta esta com saldo negativo")
 	}
 }
